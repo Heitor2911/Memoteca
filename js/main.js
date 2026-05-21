a5faa1a54377b4453ed7fc20fb860b4ba7e63547
Heitor Lacourt <heitorlmsantos@gmail.com>
2026-05-21 14:42:16 -0300

    Adiciona a funcionalidade de cadastro de pensamentos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const formPensamento = document.getElementById('pensamento-form')
+    formPensamento.addEventListener('submit', manipularSubmitFormulario)
 })
+
+async function manipularSubmitFormulario(event) {
+    event.preventDefault()
+    const id = document.getElementById('pensamento-id').value
+    const conteudo = document.getElementById('pensamento-conteudo').value
+    const autoria = document.getElementById('pensamento-autoria').value
+
+    try {
+        await api.salvarPensamentos({ conteudo, autoria})
+        ui.renderizarPensamentos()
+    } 
+    catch {
+        alert('Erro ao salvar pensamento.')
+    }
+}
--- js/ui.js
+++ js/ui.js
@@ -6,19 +6,36 @@ const ui = {
 
         try {
             const pensamentos = await api.buscarPensamentos()
-            pensamentos.forEach(pensamento => {
-                listaPensamentos.innerHTML += `
-                <li class="li-pensamento" data-id="${pensamento.id}">
-                <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-                <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-                <div class="pensamento-autoria">${pensamento.autoria}</div>
-                </li>
-                `
-            });
+            pensamentos.forEach(ui.adicionarPensamento);
         }
         catch {
             alert('Erro ao renderizar pensamentos.')
         }
+    },
+
+    async adicionarPensamento(pensamento) {
+        const listaPensamentos = document.getElementById('lista-pensamentos')
+        const li = document.createElement('li')
+        li.setAttribute('data-id', pensamento.id)
+        li.classList.add('li-pensamento')
+
+        const iconeAspas = document.createElement('img')
+        iconeAspas.src = 'assets/imagens/aspas-azuis.png'
+        iconeAspas.alt = 'Aspas azuis'
+        iconeAspas.classList.add('icone-aspas')
+
+        const conteudoPensamento = document.createElement('div')
+        conteudoPensamento.textContent = pensamento.conteudo
+        conteudoPensamento.classList.add('pensamento-conteudo')
+
+        const autoriaPensamento = document.createElement('div')
+        autoriaPensamento.textContent = pensamento.autoria
+        autoriaPensamento.classList.add('pensamento-autoria')
+
+        li.appendChild(iconeAspas)
+        li.appendChild(conteudoPensamento)
+        li.appendChild(autoriaPensamento)
+        listaPensamentos.appendChild(li)
     }
 }
 
